0e8258c1d72b31b82b3e26fc32087cbe36c12a2a
priyankagorte <priyankagorte95@gmail.com>
2019-07-30 17:23:49 +0530

    Checking Year is Leap ot not

---
+++ src/com/bridgelab/functional/LeapYear.java
@@ -0,0 +1,34 @@
+/**
+*@purpose	:Check year is a Leap Year or not.
+*@author	:Priynaka Gorte
+*@version 	:1.8
+*@since		:30-07-2019
+*/
+package com.bridgelab.functional;
+
+import java.util.Scanner;
+
+import com.bridgelab.utility.Utility;
+
+public class LeapYear {
+
+	public static void main(String[] args)
+	{
+		Scanner scanner=new Scanner(System.in);
+		Utility utility=new Utility();
+		int mYear;
+		
+		do
+		{
+			System.out.println("Enter Four Digit the Year");
+			mYear=scanner.nextInt();
+		}while(!utility.isFourDigit(mYear));
+		
+		if(mYear%4==00|| mYear%100==0 && mYear%400==00)
+			System.out.println(mYear+ " is Leap Year");
+		else
+			System.out.println(mYear+ " is NOT Leap Year");
+
+
+}
+}
---
+++ src/com/bridgelab/utility/Utility.java
@@ -0,0 +1,15 @@
+package com.bridgelab.utility;
+
+public class Utility {
+/*
+ * @purpose	:Check Year is four Digit of not
+ */
+	public boolean isFourDigit(int year)
+	{
+		if(year>=1000 && year<=9999)
+			return true;
+		
+		return false;
+			
+	}
+}
